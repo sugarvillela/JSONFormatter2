d51095fb9e72ce9df3d7ff1cef15404f98a0e4c2
Dave Swanson <davesswanson@hotmail.com>
2025-06-18 14:45:16 -0700

    Update diff.js

--- js/diff.js
+++ js/diff.js
@@ -1,107 +1,140 @@
-//console.log("diff js");
+const DiffRow = (() => {
+    /* private */ const fnv1a32 = (str) => {
+        let hash = 2166136261; // FNV_prime_32
+        for (let i = 0; i < str.length; i++) {
+            hash ^= str.charCodeAt(i);
+            hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
+        }
+        return hash;
+    };
+    
+    /* private */ const rowProto = () => ({
+        toksRow: [],
+        toksKeys: [],
+        toksVals: [],
+        value: "",
+        hashRow: 0,
+        hashSymb: 0,
+        hashKey: 0,
+        hashVal: 0
+    });
+    
+    const setHashesOne = (row) => {
+        row.value = row.toksRow.map(t => t.value).join("");
+        row.hashRow = fnv1a32(row.value);
+        
+        row.hashSymb = fnv1a32(
+            row.toksRow
+                .filter(t => !t.isValue && !t.isKey)
+                .map(t => t.value)
+                .join("")  
+        );
+    
+        row.toksKeys = row.toksRow
+            .filter(t => t.isKey);
+    
+        row.hashKey = fnv1a32(
+            row.toksKeys
+                .map(t => t.value)
+                .join("")    
+        );
+        
+        row.toksVals = row.toksRow
+            .filter(t => t.isValue);
+    
+        row.hashVal = fnv1a32(
+            row.toksVals
+                .map(t => t.value)
+                .join("")    
+        );
+    };
+    
+    const toDiffRows = (tokens) => {       
+        let isEndline = false;
+        let diffRows = [];
+        let row = rowProto();
 
-//const testData = {
-//	"pointer":1,
-//	"stack":[
-//            "{\"f0\":[1,2,\"a\"],\"f1\":{\"f2\":true}}",
-//            "{\"f0\":[1,2,\"b\"],\"f1\":{\"f2\":true}}",
-//            "{\"f0\":[1,2,\"b\"],\"f1\":{\"f2\":false}}",
-//            "{\"f0\":[1,3,\"b\"],\"f1\":{\"f2\":false}}"
-//	]
-//};
+        tokens.forEach(t => {          
+            if(t.checkEndline && isEndline){
+                if(row.toksRow.length){
+                    diffRows.push(row);
+                }
+                row = rowProto();
+            }
+            row.toksRow.push(t);
+
+            isEndline = t.isEndline;
+        });
+        
+        if(row.toksRow.length){//push remaining
+            diffRows.push(row);
+        }
+        
+        diffRows.forEach(row => {
+            setHashesOne(row);
+        });
+        
+        return diffRows;
+    };
+    
+    return {
+        toDiffRows: (tokens) => toDiffRows(tokens)
+    };
+})();
 
 const DiffGen = (() => {
-    const disp = (label, toks) => {
-        console.log("==" + label+"==")
-        toks.forEach(t => console.log(t.className + " " + t.value))
-    }
-    const getSymbols = (diffRow) => {
-        return diffRow.tokens.filter(t => !t.isValue && t.className !== "charKey")
-                .map(t => t.value).join("");
-    };
-    const getKeys = (diffRow) => {
-        return diffRow.tokens.filter(t => t.isKey);
-    };
-    const getValues = (diffRow) => {
-        return diffRow.tokens.filter(t => t.isValue);
-    };
-    const compareEqualLengths = (tokensL, tokensR) => {
-        for(let i = 0; i < tokensL.length; i++){
-            if(tokensL[i].value !== tokensR[i].value){
-                //console.log(`${tokensL[i].value} !== ${tokensR[i].value}`)
-                return false;
-            }
-        }
-        //console.log(`${tokensL[0].value} === ${tokensR[0].value}`)
-        return true;
-    };
-    const compare = (tokensL, tokensR) => {
-        return (tokensL.length === tokensR.length) && compareEqualLengths(tokensL, tokensR);  
-    };
     const addErrClass = (tokens) => {
         tokens.forEach(t => t.className = "diffToken");
     };
 
-    const similar = (tokL, tokR) => {
-        //console.log(`${getSymbols(tokL)} ?== ${getSymbols(tokR)}`)
-        if(tokL.hash === tokR.hash){
+    const similar = (rowL, rowR) => {
+        if(rowL.hashRow === rowR.hashRow){
             return true;
         }
         
-        if(getSymbols(tokL) !== getSymbols(tokR)){
+        if(rowL.hashSymb !== rowR.hashSymb){
             return false;
         }
-        const keysL = getKeys(tokL);
-        const keysR = getKeys(tokR);
-        const valuesL = getValues(tokL);
-        const valuesR = getValues(tokR);
+        
+        if(rowL.hashKey === rowR.hashKey){
+            if(rowL.hashVal !== rowR.hashVal){
+                addErrClass(rowL.toksVals);
+                addErrClass(rowR.toksVals);
+            }
+            return true;
+        }
+        
+        if(rowL.hashVal === rowR.hashVal){
+            if(rowL.hashKey !== rowR.hashKey){
+                addErrClass(rowL.toksKeys);
+                addErrClass(rowR.toksKeys);
+            }
+            return true;
+        }
 
-//        disp("keysL", keysL)
-//        disp("keysR", keysR)
-        //console.log("keysMatch?", keysL, keysR)
-        if(compare(keysL, keysR)){
-            //console.log("yes", keysL, keysR)
-            if(!compare(valuesL, valuesR)){
-                addErrClass(valuesL);
-                addErrClass(valuesR);
-            }
-            return true;
-        }
-        if(compare(valuesL, valuesR)){
-            if(!compare(keysL, keysR)){
-                addErrClass(keysL);
-                addErrClass(keysR);
-            }
-            return true;
-        }
         return false;
     };
 
-    const find = (list, tokL, k) => {
+    const find = (list, rowL, k) => {
         if(k < list.length){
-//        disp("tokL", tokL.tokens)
-//        disp("tokR", list[k].tokens)
-            return similar(tokL, list[k]);
+            return similar(rowL, list[k]);
         }
         return false;
     };
 
-
-    const lookAhead = (list, tokL, start) => {
+    const lookAhead = (list, rowL, start) => {
         for(let k = start; k < list.length; k++){
-            if(similar(tokL, list[k])){
-                //console.log(`lookAhead ${} == ${}`)
+            if(similar(rowL, list[k])){
                 return true;
             }
         }
-
         return false;
     };
+    
     const nullRow = {
-        tokens: [TokenGen.genNullToken()],
-        value: "_",
-        hash: 0
+        toksRow: [TokenGen.genNullToken()]
     };
+    
     let l = [];
     let r = [];
 
@@ -114,50 +147,42 @@ const DiffGen = (() => {
         let i = 0, j = 0;
 
         while (i < l.length){
-            let tokL = l[i];
+            let rowL = l[i];
 
-            if(find(r, tokL, j)){
-                destL.push(tokL);//merge l and r elements
+            if(find(r, rowL, j)){
+                destL.push(rowL);//merge l and r elements
                 destR.push(r[j]);//merge l and r elements
                 i++;
                 j++;
             }
-            else if(lookAhead(r, tokL, j)){// pause l, advance r
-                //console.log("a", tokL.value, r[j].value )
+            else if(lookAhead(r, rowL, j)){// pause l, advance r
                 destL.push(nullRow);
                 destR.push(r[j]);
                 j++;
             }
             else {// pause r, advance l
-                //console.log("b", tokL.value, r[j].value )
-                destL.push(tokL);
+                destL.push(rowL);
                 destR.push(nullRow);
                 i++;
-                //j++;
             }
         }
 
         while (j < r.length){// add any remaining r
-            //console.log("c", r[j].value )
             destL.push(nullRow);
             destR.push(r[j]);
             j++;
         }
         
-        //console.log(destL, destR);
         return [
-            destL.flatMap(d => d.tokens), 
-            destR.flatMap(d => d.tokens)
+            destL.flatMap(d => d.toksRow), 
+            destR.flatMap(d => d.toksRow)
         ];
     };
 
     return {
-            //setData: (left, right) => { l = left; r = right; },
-            gen: (diffTokens) => gen(diffTokens)
+        gen: (diffTokens) => gen(diffTokens)
     };
 })();
-
-const PlainTextFormatter = getPlainTextFormatter();
 
 const DualFormatter = (() => {
     const formatters = [
@@ -166,7 +191,6 @@ const DualFormatter = (() => {
     ];
     
     return {
-        //drawFormattedHtml: (i, tokens, jsonErrs, warnings) => formatters[i].drawFormattedHtml(tokens, jsonErrs, warnings),
         getFormatter: (i) => formatters[i]
     };
 })();
@@ -177,19 +201,12 @@ const DualTokenSource = (() => {
         getTokenSource(DualFormatter.getFormatter(1))
     ];
     
-//    const getTokensForDiff = () => {
-//        return [
-//            tokenSources[0].getTokens(),
-//            tokenSources[1].getTokens()
-//        ];
-//    }
+    DualFormatter.getFormatter(0).setTokenSource(tokenSources[0]);
+    DualFormatter.getFormatter(1).setTokenSource(tokenSources[1]);
     
     return {
         setTokens: (i, jsonTokens) => tokenSources[i].setTokens(jsonTokens),
-        haveTokens: (i) => tokenSources[i].haveTokens(),
-        getTokens: (i) => tokenSources[i].getTokens(),
-        //getTokensForDiff: () => getTokensForDiff(),
-        formattedToArray: (i) => tokenSources[i].formattedToArray()
+        getTokens: (i) => tokenSources[i].getTokens()
     };
 })();
 
@@ -198,18 +215,10 @@ const DualParser = (() => {
         let text = PreParser.preParse(origText);
 
         const [jsonTokens, jsonErrs] = JsonParser.parse(text);
-
-        //console.log("jsonErrs 1", jsonErrs);
-        //console.log(jsonTokens);
         DualTokenSource.setTokens(i, jsonTokens);
         
         let tokens = DualTokenSource.getTokens(i);
-        return PlainTextFormatter.toDiffRows(tokens);
-        //let formatted = DualTokenSource.formattedToArray(i);
-        //console.log("tokens", tokens);
-        
-        //DualFormatter.getFormatter(i).drawFormattedHtml(tokens, jsonErrs, PreParser.getWarnings());
-        //return formatted;
+        return DiffRow.toDiffRows(tokens);
     };
     const parseArr = (textArr) => {
         const diffRows = [
@@ -217,11 +226,8 @@ const DualParser = (() => {
             parse(1, textArr[1])
         ];
         
-        //console.log(formatted)
-        
-//        let diffToks = DualTokenSource.getTokensForDiff();
         const newDiffRows = DiffGen.gen(diffRows);
-//        
+ 
         DualFormatter.getFormatter(0).drawFormattedHtml(newDiffRows[0], [], []);
         DualFormatter.getFormatter(1).drawFormattedHtml(newDiffRows[1], [], []);
     };
@@ -267,16 +273,13 @@ const SelectUtil = (() => {
         initOne(1, options, selected[1]);
         
         const strings = DiffState.getStrings(selected);
-        //console.log("strings", strings);
         DualParser.parseArr(strings);
     };
     
     const onChange = (index) => {
         const value = document.getElementById(`select${index}`).value;
         selected[index] = value;
-        //console.log("change", index, value);
         const strings = DiffState.getStrings(selected);
-        //console.log("strings", strings);
         DualParser.parseArr(strings);
     };
 
@@ -308,14 +311,16 @@ const DiffState = (() => {// for diff.html state
 
     const initDiffFromStorage = () => {
         initFromStorage();
-        //console.log("init d", state)
-        //DomProxy.renderState(state);
-        //DomProxy.renderDiffButton(state.stack.length < 2);
         
         if(state.stack.length > 1){
             SelectUtil.init(state);
-//            DomProxy.setOrigTextValue(state.stack[state.pointer]);
-//            Parser.parse();
+        }
+        else {
+            const diff0 = document.getElementById("diff0");
+            const diff1 = document.getElementById("diff1");
+
+            DualFormatter.getFormatter(0).appendWarn(diff0, "Need two or more items for diff...");
+            DualFormatter.getFormatter(1).appendWarn(diff1, "Add content in JSONFormatter and refresh this page");
         }
     };
     
@@ -325,43 +330,3 @@ const DiffState = (() => {// for diff.html state
         getStrings: (selected) => [state.stack[selected[0]],state.stack[selected[1]]]
     };
 })();
-
-
-
-
-//const testGen = () => {
-//    const dLeft1 = ['a','b','e','f'];
-//    const dRight1 = ['a','b','c','d','e','f'];
-//    // ['a','b','c','d','e','f']
-//
-//    const dLeft2 = ['a','b','c','d','e','f'];
-//    const dRight2 = ['a','b','e','f'];
-//    // ['a','b','c','d','e','f']
-//
-//    const dLeft3 = ['a','b','c','d'];
-//    const dRight3 = ['e','f','g','h'];
-//    // ['a','b','c','d','e','f','g','h']
-//
-//    const dLeft4 = ['e','f','g','h'];
-//    const dRight4 = ['a','b','c','d'];
-//    // ['e','f','g','h', 'a','b','c','d'] // oops, left side wins
-//
-//    const dLeft5 = ['a','b','c','d'];
-//    const dRight5 = ['c','x','d','z'];
-//    // ['a','b','c','x','d','z']
-//
-//    DiffGen.setData(dLeft1, dRight1);
-//    DiffGen.gen();
-//    DiffGen.setData(dLeft2, dRight2);
-//    DiffGen.gen();
-//    DiffGen.setData(dLeft3, dRight3);
-//    DiffGen.gen();
-//    DiffGen.setData(dLeft4, dRight4);
-//    DiffGen.gen();
-//    DiffGen.setData(dLeft5, dRight5);
-//    DiffGen.gen();
-//};
-//
-//testGen();
-
-
